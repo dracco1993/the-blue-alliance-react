c1c73d9b69dd40e0ffe16e03b08e0895b46c4d21
Jordan Miller <jmiller@springbuk.com>
2019-06-29 12:26:15 -0400

    Added double check mark for double rocket

--- app/views/breakdowns/MatchBreakdown.js
+++ app/views/breakdowns/MatchBreakdown.js
@@ -1,39 +1,34 @@
-import React from 'react';
-import ReactNative from 'react-native';
-import breakdownStyle from '../../styles/breakdown';
-import images from '../../config/images';
+import React from "react";
+import ReactNative from "react-native";
+import breakdownStyle from "../../styles/breakdown";
+import images from "../../config/images";
 
 // Override our Image and Text to have specific sizes
-const Image = ({ style, ...props }) => <ReactNative.Image style={[breakdownStyle.imageSize, style]} {...props} />;
+const Image = ({ style, ...props }) => (
+  <ReactNative.Image style={[breakdownStyle.imageSize, style]} {...props} />
+);
 
 export default class MatchBreakdown extends React.Component {
   checkImage() {
-    return (
-      <Image source={images.check} />
-    );
+    return <Image source={images.check} />;
+  }
+  doubleCheckImage() {
+    return <Image source={images.doubleCheck} />;
   }
 
   checkCircleImage() {
-    return (
-      <Image source={images.checkCircle} />
-    );
+    return <Image source={images.checkCircle} />;
   }
 
   xImage() {
-    return (
-      <Image source={images.clear} />
-    );
+    return <Image source={images.clear} />;
   }
 
   upArrowImage() {
-    return (
-      <Image source={images.arrows.up} />
-    );
+    return <Image source={images.arrows.up} />;
   }
 
   downArrowImage() {
-    return (
-      <Image source={images.arrows.down} />
-    );
+    return <Image source={images.arrows.down} />;
   }
 }
